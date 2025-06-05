8bab2399dc8ffd5e9a23b1a8fd49301fb06a65e8
izabely <izabelydaluzfernandes@gmail.com>
2025-06-04 21:49:15 -0300

    Add files via upload

---
+++ aula 15/contador-e-diminuidor.ino
@@ -0,0 +1,43 @@
+#define pin_led1 8
+#define pin_led2 9
+#define pin_led3 10
+#define pin_led4 11
+#define pin_botaoA 2
+#define pin_botaoB 3
+int cont = 0;
+bool aux = 0;
+
+
+void setup() {
+  pinMode(pin_led1, OUTPUT);
+  pinMode(pin_led2, OUTPUT);
+  pinMode(pin_led3, OUTPUT);
+  pinMode(pin_led4, OUTPUT);
+  pinMode(pin_botaoA, INPUT);
+  pinMode(pin_botaoB, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  bool botaoA = digitalRead(pin_botaoA);
+  bool botaoB = digitalRead(pin_botaoB);
+  // contagem inversa - DOW e up
+  if (botaoA == 1) {
+    if (aux == 0) {
+      cont++;
+      aux = 1;
+    }
+  } else {
+    aux = 0;
+  }
+  Serial.println(cont);
+  if (botaoB == 1) {
+    if (aux == 0) {
+      cont--;
+      aux = 1;
+    }
+  } else {
+    aux = 0;
+  }
+  Serial.println(cont);
+}
