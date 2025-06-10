d1c9ac3481baaba1ba13c23aaa4d883e1982bde6
izabely <izabelydaluzfernandes@gmail.com>
2025-06-10 13:27:29 -0300

    Update limites.ino

--- aula 15/limites.ino
+++ aula 15/limites.ino
@@ -43,4 +43,14 @@ void loop() {
     aux = 0;
   }
   Serial.println(cont);
+} if cont == 50 {
+  digitalWrite(led1, HIGH);
+} else {
+  digitalWrite(led1, LOW);
+ }
+} if cont == 0 {
+  digitalWrite(led2, HIGH);
+} else {
+  digitalWrite(led2, LOW);
+ }
 }
